3bc7fd3621dc566285d04c4e8c2e53e0dc1e2338
xuanphi <doanxuanphi2@gmail.com>
2025-04-04 22:21:01 +0700

    renamed App

--- src/main/java/com/wiinvent/lotus/checkin/checkin/CheckInApplication.java
+++ src/main/java/com/wiinvent/lotus/checkin/checkin/CheckInApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheckinApplication {
+public class CheckInApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CheckinApplication.class, args);
+		SpringApplication.run(CheckInApplication.class, args);
 	}
 
 }
