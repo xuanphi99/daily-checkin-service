a7116c2da1592dc656d2b83fdad99cc17e126d2f
xuanphi <doanxuanphi2@gmail.com>
2025-04-04 15:14:16 +0700

    init service

---
+++ src/test/java/com/wiinvent/lotus/checkin/checkin/CheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiinvent.lotus.checkin.checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
